ca73608cd3979b2bdfc597396e3826567df183d4
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-25 14:33:52 +0100

    Done Snack 1

--- main.js
+++ main.js
@@ -2,6 +2,39 @@
 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore. */
 
+const bici = [
+    {
+        nome: "Bici A",
+        peso: 10
+    },
+    {
+        nome: "Bici B",
+        peso: 1
+    },
+    {
+        nome: "Bici C",
+        peso: 5
+    },
+    {
+        nome: "Bici D",
+        peso: 3
+    }
+]
+
+console.log(bici);
+
+let biciLeggera = bici[0]
+
+for (let index = 0; index < bici.length; index++) {
+    if (bici[index].peso < biciLeggera.peso) {
+        biciLeggera = bici[index]
+    }
+}
+
+console.log(`La bici più leggera è: ${biciLeggera.nome} e pesa ${biciLeggera.peso}`);
+
+
+
 /* Snack2
 Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
